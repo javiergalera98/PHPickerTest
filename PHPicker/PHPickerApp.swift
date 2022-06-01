dab508bf57df4982952574ede546eba1cdee1bf9
Javier Galera Robles <jgalerarobles@icloud.com>
2022-06-01 18:29:58 +0200

    Initial Commit

---
+++ PHPicker/PHPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PHPickerApp.swift
+//  PHPicker
+//
+//  Created by Javier Galera Robles on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PHPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
