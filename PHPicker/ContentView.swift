b746e5419025afd64a1b42743dc58ecefba9fad8
Javier Galera Robles <jgalerarobles@icloud.com>
2022-06-01 19:03:43 +0200

    Project finished

--- PHPicker/ContentView.swift
+++ PHPicker/ContentView.swift
@@ -6,11 +6,49 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var isPresented: Bool = false
+    @State var selectedImage: UIImage?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if let selectedImage = selectedImage {
+                Image(uiImage: selectedImage)
+                    .resizable()
+                    .frame(width: 300.0, height: 300.0, alignment: .center)
+                    .cornerRadius(30.0)
+            } else {
+                VStack {
+                    Spacer()
+                    Text("No image selected")
+                        .foregroundColor(.secondary)
+                    Spacer()
+                }
+            }
+            
+            Spacer()
+            
+            Button("Select photo") {
+                isPresented.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .sheet(isPresented: $isPresented) {
+            let configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+            PhotoPicker(configuration: configuration) { image in
+                selectedImage = image
+            }
+        }
+    }
+    
+    // Add PHPicker configuration
+    var pickerConfiguration: PHPickerConfiguration {
+        var config = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+        config.filter = .images
+        config.selectionLimit = 10
+        return config
     }
 }
 
